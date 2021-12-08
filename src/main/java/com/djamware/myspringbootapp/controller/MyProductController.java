c542954bcfb0158a1480908026b3212a00b21376
Farhan <farhan96.hm@gmail.com>
2021-12-08 14:19:59 +0600

    Change in git message

--- src/main/java/com/djamware/myspringbootapp/controller/MyProductController.java
+++ src/main/java/com/djamware/myspringbootapp/controller/MyProductController.java
@@ -34,6 +34,6 @@ public class MyProductController {
 
     @GetMapping("/git")
     public @ResponseBody String testController(){
-        return "Hello Gitty";
+        return "Hello Git";
     }
 }
